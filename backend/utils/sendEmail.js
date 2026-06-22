cb85cda6125c4b10eee645f8d755c757f3bd98fc
FakeCoder420 <jainsahabreal1605@gmail.com>
2026-06-22 12:16:09 +0530

    fixed ipv6 network routing error for nodemailer

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, text) => {
-  
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
-        secure: true,
+        secure: true, 
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+       
+        family: 4 
     });
 
     await transporter.sendMail({ 
