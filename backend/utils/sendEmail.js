f0668d6a096a95240dd6ef2574eb239f17127aa2
FakeCoder420 <jainsahabreal1605@gmail.com>
2026-06-22 12:10:47 +0530

    fixed nodemailer connection timeout

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,15 +1,23 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, text) => {
+  
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
     });
 
-    await transporter.sendMail({ from: process.env.EMAIL_USER, to, subject, text });
+    await transporter.sendMail({ 
+        from: process.env.EMAIL_USER, 
+        to, 
+        subject, 
+        text 
+    });
 };
 
 module.exports = sendEmail;
